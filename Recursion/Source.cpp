dfc9e4521e03e8c9144e625724ff56e99f278430
Aleksey <sav459811@mail.ru>
2022-03-17 21:19:01 +0500

    Recursion F

---
+++ Recursion/Source.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+using namespace std;
+
+void elevator(int floor)
+{
+	if (floor == 0)
+	{
+		cout << "Нулевой этаж" << endl;
+		return;
+	}
+	cout << " " << floor << " Этаж" << endl;
+	elevator(floor - 1);
+	cout << "Вы на " << floor << " этаже" << endl;
+}
+
+int factorial(int n)
+{
+	
+	if (n == 0 || n==1) return 1;
+	return n * factorial(n - 1);
+
+}
+	
+void main()
+{
+	setlocale(LC_ALL, "");
+	//cout << "Hello";
+	//main();
+	int floor;
+	int F;
+	/*cout << "ВВедите на какой этаж: "; cin >> floor;
+	elevator(floor);*/
+
+	cout << "Введите число факториал n!=1*2*n! : "; cin >> F;
+	cout << factorial(F);
+
+}
